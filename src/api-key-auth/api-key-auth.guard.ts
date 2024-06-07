2c53c8a7d0fbd7929404eb843c0ab33f1502fde3
MingNe <minhnq.0702@gmail.com>
2024-06-07 11:41:15 +0700

    add api auth guard

---
+++ src/api-key-auth/api-key-auth.guard.ts
@@ -0,0 +1,10 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiKeyAuthGuard implements CanActivate {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    return true;
+  }
+}
