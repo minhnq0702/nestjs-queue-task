786d7faca1fef165cd9f1462683804cc15a2a2fe
MingNe <minhnq.0702@gmail.com>
2024-06-07 11:48:15 +0700

    update test config

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    // '^image![a-zA-Z0-9$_-]+$': 'GlobalImageStub',
+    // '^[./a-zA-Z0-9$_-]+\\.png$': '<rootDir>/RelativeImageStub.js',
+    // 'module_name_(.*)': '<rootDir>/substituted_module_$1.js',
+    // 'assets/(.*)': [
+    //   '<rootDir>/images/$1',
+    //   '<rootDir>/photos/$1',
+    //   '<rootDir>/recipes/$1',
+    // ],
+    '@/(.*)': '<rootDir>/src/$1',
+  },
+};
+
+export default config;
