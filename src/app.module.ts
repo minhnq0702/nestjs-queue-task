fd5ca76400ebb47704bd13afa818337b94b7215d
MingNe <minhnq.0702@gmail.com>
2024-06-07 11:49:20 +0700

    change path by @

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { AuthModule } from '@/auth/auth.module';
+import { KafkaModule } from '@/kafka/kafka.module';
+import { LoggerModule } from '@/logger/logger.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { KafkaModule } from './kafka/kafka.module';
-import { LoggerModule } from './logger/logger.module';
 
 @Module({
-  imports: [LoggerModule.register('RootApp'), KafkaModule],
+  imports: [LoggerModule.register('RootApp'), KafkaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
