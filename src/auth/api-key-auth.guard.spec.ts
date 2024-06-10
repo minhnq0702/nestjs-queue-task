db9d9dbcf57a6f6b86447ede000b09fad397a680
MingNe <minhnq.0702@gmail.com>
2024-06-10 15:21:56 +0700

    fix test and format code

--- src/auth/api-key-auth.guard.spec.ts
+++ src/auth/api-key-auth.guard.spec.ts
@@ -1,7 +1,19 @@
+import { LoggerService } from '@/logger/logger.service';
+import { Test, TestingModule } from '@nestjs/testing';
 import { ApiKeyAuthGuard } from './api-key-auth.guard';
 
 describe('ApiKeyAuthGuard', () => {
+  let apiKeyAuthGuard: ApiKeyAuthGuard;
+
+  beforeAll(async () => {
+    const loggerModule: TestingModule = await Test.createTestingModule({
+      providers: [LoggerService],
+      exports: [LoggerService]
+    }).compile();
+    apiKeyAuthGuard = new ApiKeyAuthGuard(loggerModule.get<LoggerService>(LoggerService));
+  });
+
   it('should be defined', () => {
-    expect(new ApiKeyAuthGuard()).toBeDefined();
+    expect(apiKeyAuthGuard).toBeDefined();
   });
 });
