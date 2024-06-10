5b62b0dddf3107875f336557f35ca2b330963e57
MingNe <minhnq.0702@gmail.com>
2024-06-10 21:16:33 +0700

    add kafka validator exception filter

---
+++ src/kafka/kafka.filter.spec.ts
@@ -0,0 +1,7 @@
+import { KafkaFilter } from './kafka.filter';
+
+describe('KafkaFilter', () => {
+  it('should be defined', () => {
+    expect(new KafkaFilter()).toBeDefined();
+  });
+});
---
+++ src/kafka/kafka.filter.ts
@@ -0,0 +1,21 @@
+import { LoggerService } from '@/logger/logger.service';
+import { ArgumentsHost, BadRequestException, Catch, ExceptionFilter } from '@nestjs/common';
+import { KafkaContext } from '@nestjs/microservices';
+
+@Catch(BadRequestException)
+export class KafkaFilter implements ExceptionFilter {
+  constructor(private readonly logger: LoggerService) {}
+  async catch(exception: BadRequestException, host: ArgumentsHost) {
+    const ctx = host.switchToRpc().getContext<KafkaContext>();
+    const heartbeat = ctx.getHeartbeat();
+    await heartbeat();
+    const topic = ctx.getTopic();
+    const partition = ctx.getPartition();
+    const offset = ctx.getPartition();
+
+    this.logger.error(
+      `Validation error on topic ${topic}, partition ${partition}, offset ${offset}: ${exception.message}`,
+      exception.stack
+    );
+  }
+}
