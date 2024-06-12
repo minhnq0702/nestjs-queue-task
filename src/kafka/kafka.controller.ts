c0b8be9e2b8ee511d151f3876b0ac14e2ca6b01e
MingNe <minhnq.0702@gmail.com>
2024-06-12 12:23:30 +0700

    update controller

--- src/kafka/kafka.controller.ts
+++ src/kafka/kafka.controller.ts
@@ -8,13 +8,17 @@ export class KafkaController {
   constructor(@Inject('KAFKA_ODOO_TASK') private readonly client: ClientKafka) {}
 
   @EventPattern('dev-test') // ! use eventpattern instead of messagepattern for no reply needed
-  @UsePipes(new ValidationPipe({ transform: true }))
+  @UsePipes(ValidationPipe)
   @UseFilters(KafkaFilter)
   async devTest(@Payload() messages: TaskDto, @Ctx() ctx: KafkaContext): Promise<Record<string, string>> {
-    console.log('Received message:', messages);
+    // const t = plainToClass(Task, messages);
+    // console.log('Transformed message:', t);
+
     const heartbeat = ctx.getHeartbeat();
     await heartbeat();
-    this.client.emit('dev_1', JSON.stringify({ status: 'ok' }));
+
+    // ! Dev testing
+    // this.client.emit('dev_1', JSON.stringify({ status: 'ok' }));
     return { status: 'ok' };
   }
 
