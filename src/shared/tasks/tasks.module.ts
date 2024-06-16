0a216c83efd67be47185b292abbda6de1aaafe3b
MingNe <minhnq.0702@gmail.com>
2024-06-16 23:19:23 +0700

    call process task

--- src/shared/tasks/tasks.module.ts
+++ src/shared/tasks/tasks.module.ts
@@ -1,6 +1,8 @@
 import { Task, TaskSchmea } from '@/entities/task.entity';
+import { ExternalModule } from '@/external/external.module';
 import { LoggerModule } from '@/logger/logger.module';
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
@@ -8,9 +10,10 @@ import { TasksService } from './tasks.service';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Task.name, schema: TaskSchmea }]),
-    LoggerModule.register('Shared/Tasks')
+    LoggerModule.register('Shared/Tasks'),
+    ExternalModule
   ],
-  providers: [TasksService],
+  providers: [ConfigService, TasksService],
   controllers: [TasksController]
 })
 export class TasksModule {}
--- src/shared/tasks/tasks.service.ts
+++ src/shared/tasks/tasks.service.ts
@@ -1,7 +1,10 @@
+import { ODOO_CONFIG } from '@/constants';
 import { GetDomain } from '@/entities/base.entity';
 import { Task, TaskDocument, TaskOperation, TaskStateEnum } from '@/entities/task.entity';
+import { OdooService } from '@/external/odoo/odoo.service';
 import { LoggerService } from '@/logger/logger.service';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
@@ -9,7 +12,9 @@ import { Model } from 'mongoose';
 export class TasksService {
   constructor(
     @InjectModel(Task.name) private taskModel: Model<Task>,
-    private readonly logger: LoggerService
+    private readonly logger: LoggerService,
+    private readonly config: ConfigService,
+    private readonly odooService: OdooService // ? should change to externalService and use Odoo as a functional service
   ) {}
 
   // ? review type Promise<TaskDocument[]>
@@ -40,6 +45,17 @@ export class TasksService {
         throw new Error('Task not found');
       }
       this.logger.debug(`# TODO: Execute task ${task._id}`);
+      this.odooService.callDoingTask({
+        url: this.config.get(ODOO_CONFIG.ODOO_URL),
+        db: this.config.get(ODOO_CONFIG.ODOO_DB),
+        user: this.config.get(ODOO_CONFIG.ODOO_HTTP_USER) || null,
+        pass: this.config.get(ODOO_CONFIG.ODOO_HTTP_PASSWORD) || null,
+        model: task.model,
+        func: task.func,
+        records: task.records,
+        args: task.args,
+        kwargs: task.kwargs
+      });
       return task;
     });
   }
