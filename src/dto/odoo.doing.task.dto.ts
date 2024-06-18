74c11c471b58f4c58b71b92d652b598defe52b5a
MingNe <minhnq.0702@gmail.com>
2024-06-18 11:04:04 +0700

    update dto odoo doing task

---
+++ src/dto/odoo.doing.task.dto.ts
@@ -0,0 +1,13 @@
+// ? Is it should be here?
+export type OdooDoingTaskParams = {
+  dbId: string;
+  url: string;
+  db: string;
+  user: string;
+  pass: string;
+  model: string;
+  func: string;
+  records: string;
+  args: string;
+  kwargs: string;
+};
