ce9ebd3ec1e73df835cb2d332e9ff3f504241038
MingNe <minhnq.0702@gmail.com>
2024-07-04 14:54:50 +0700

    add auth controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { HttpAuthGuard } from '@/common/http.auth.guard';
 import { LoggerModule } from '@/logger/logger.module';
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
@@ -10,8 +11,9 @@ import { AuthService } from './auth.service';
     AuthService,
     {
       provide: APP_GUARD, // * Register auth guard for global
-      useClass: HttpAuthGuard
-    }
-  ]
+      useClass: HttpAuthGuard,
+    },
+  ],
+  controllers: [AuthController],
 })
 export class AuthModule {}
