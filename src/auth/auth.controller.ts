6b1b6767b72fce8c24e6ca5746143364b3086021
MingNe <minhnq.0702@gmail.com>
2024-07-12 23:29:15 +0700

    add api get profile

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
+import { JwtKey } from '@/common/http.auth.guard';
 import { LoginDto } from '@/dto';
-import { Body, Controller, HttpCode, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthService } from './auth.service';
 
@@ -10,12 +11,18 @@ export class AuthController {
   @Post('login')
   @HttpCode(200)
   // use passthrough = true to apply both way @nestjs and @express
-  async login(@Body() payload: LoginDto, @Res({ passthrough: true }) resp: Response) {
+  async ctrlLogin(@Body() payload: LoginDto, @Res({ passthrough: true }) resp: Response) {
     const token = await this.authSvc.authenticate(payload.login, payload.password);
     // * use express response to set cookie
     resp.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'lax' });
 
     // * leave the response to be handled by nestjs
     return 'Logged in successfully!';
+  }
+
+  @Get('profile')
+  @JwtKey()
+  async ctrlGetProfile() {
+    return 'Profile';
   }
 }
